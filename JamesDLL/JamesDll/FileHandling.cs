6301f7869577b6ffc850e366399e77cf6c946fc9
James Cairns <james.cairns@outlook.com>
2021-09-13 15:16:58 +0100

    Created First Methods
    Two files each one containing methods which pertain to actions related to their names.

---
+++ JamesDLL/JamesDll/FileHandling.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace JamesDll
+{
+    public class FileHandling
+    {
+        public static string GetFileType(string path)
+        {
+            string fileType = "";
+
+            string[] pathSplit = path.Split('.');
+
+            return fileType = "." + pathSplit.Last();
+        }
+
+        public static void GetFolderPath(out string folderPath)
+        {
+            Console.WriteLine("Please enter the path to your folder.");
+            Console.Write(": ");
+            string ans = Console.ReadLine();
+
+            if (!ans.EndsWith("\\"))
+                ans = ans + "\\";
+
+            if (Directory.Exists(ans) && Directory.GetFiles(ans).Length > 0)
+            {
+                folderPath = ans;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("That folder is empty or does not exist. Try again.");
+                GetFolderPath(out folderPath);
+            }
+        }
+
+        public static string FileChoice()
+        {
+            Console.Write("Please enter the name of your file: ");
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ JamesDLL/JamesDll/Time.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace JamesDll
+{
+    public class Time
+    {
+        public static void WaitForSeconds(int seconds)
+        {
+            Console.WriteLine("Waiting " + seconds + " seconds...");
+            while (Wait(TimeSpan.FromSeconds(seconds))) ;
+        }
+        public static void WaitForMinutes(int minutes)
+        {
+            Console.WriteLine("Waiting " + minutes + " minutes...");
+            while (Wait(TimeSpan.FromMinutes(minutes))) ;
+        }
+        public static void WaitForHours(int hours)
+        {
+            Console.WriteLine("Waiting " + hours + " hours...");
+            while (Wait(TimeSpan.FromHours(hours))) ;
+        }
+        private static bool Wait(TimeSpan time)
+        {
+            //TimeSpan milliseconds = seconds;
+            var stopwatch = Stopwatch.StartNew();
+
+            var lastVal = TimeSpan.Zero;
+
+            while (true)
+            {
+                var currentVal = stopwatch.Elapsed;
+                if (currentVal - lastVal < time) continue;
+
+                lastVal = currentVal;
+                break;
+            }
+            return false;
+        }
+    }
+}
